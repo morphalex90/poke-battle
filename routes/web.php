dba6acd818ddce2898d2571249ad2acba4aee0ce
morphalex90 <piero.nanni@gmail.com>
2025-06-11 02:08:51 +0100

    add pokemon

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
+    $response = Http::get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+    $pokemon = $response->json();
+
+    $pokemonList = collect($pokemon['results'])->map(function ($item) {
+        return [
+            'value' => $item['url'],
+            'label' => ucfirst($item['name']),
+        ];
+    })->toArray();
+
+    return Inertia::render('welcome', ['pokemon' => $pokemonList]);
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
